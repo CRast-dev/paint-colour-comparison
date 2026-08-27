ba2bdfb0851273193f8676903bf62a68f859d9a2
Christian Rast <christian.rast98@gmail.com>
2026-08-28 00:01:07 +0200

    hopefully correct hsv euclidian distance

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def main():
         print("faulty colour generation: ", error)
 
     rgb_euclidian_distance = calculate_rgb_euclidian_dist(colour1,colour2)
+    hsv_euclidian_distance = calculate_hsv_dist(colour1, colour2)
     print("Euclidian distance: ", rgb_euclidian_distance)
-
+    print("HSV euclidian distance: ", hsv_euclidian_distance)
 
 def calculate_rgb_euclidian_dist(colour1, colour2):
     r_component = (colour1.rgb[0] - colour2.rgb[0])**2
@@ -20,7 +21,22 @@ def calculate_rgb_euclidian_dist(colour1, colour2):
     b_component = (colour1.rgb[2] - colour2.rgb[2])**2
     return math.sqrt(r_component + g_component + b_component)
 
+def calculate_hsv_dist(colour1, colour2):
+    h1_radian = math.radians(colour1.hsv[0])
+    s1 = colour1.hsv[1] / 360.0
+    v1 = colour1.hsv[2] / 360.0
+    x1_component = math.cos(h1_radian) * s1 * v1
+    y1_component = math.sin(h1_radian) * s1 * v1
+    z1_component = v1
 
+    h2_radian = math.radians(colour2.hsv[0])
+    s2 = colour2.hsv[1] / 360.0
+    v2 = colour2.hsv[2] / 360.0
+    x2_component = math.cos(h2_radian) * s2 * v2
+    y2_component = math.sin(h2_radian) * s2 * v2
+    z2_component = v2
+
+    return math.sqrt((x1_component-x2_component)**2 + (y1_component - y2_component)**2 + (z1_component - z2_component)**2)
 
 if __name__ == "__main__":
     main()
