5914f808ac1dec2cc79cfb7d6d2b301a7c558dc2
Christian Rast <christian.rast98@gmail.com>
2026-09-02 21:32:43 +0200

    implement proper UI for both pixel and hexvalue comparison

--- app.py
+++ app.py
@@ -34,59 +34,68 @@ def display_similarity(algo_type, distance, level, colour):
 
 st.title("Paint Colour Analyzer")
 
-uploaded_image = st.file_uploader(
-    "Upload an image",
-    type=["png", "jpg", "jpeg"]
-)
-if uploaded_image is not None:
-    image = Image.open(uploaded_image).convert("RGB")
-    coordinates = streamlit_image_coordinates(image)
-    x_coord = coordinates["x"]
-    y_coord = coordinates["y"]
-    pixel = image.getpixel((x_coord, y_coord))
-    hex_code_pixel = Colour.getHexFromRGB(pixel[0],pixel[1],pixel[2])
-    st.write(f"Pixel Hexcode: {hex_code_pixel}")
 
+st.subheader("Colour to compare against")
 colour1_input = st.color_picker(
-    "Choose the colour",
-    "#C04040"
-)
-
-colour2_input = st.color_picker(
-    "Choose the colour",
-    "#C11111"
-)
+    "Choose Colour that is compared against","#C04040")
 colour1 = Colour(colour1_input)
-colour2 = Colour(colour2_input)
 
-st.subheader("Selected colours")
-col1, col2 = st.columns(2)
-with col1:
-    st.write("Colour 1")
-    st.code(colour1.hex)
-with col2:
-    st.write("Colour 2")
-    st.code(colour2.hex)
+colour2 = None
+st.subheader("Your chosen colour")
+colour2_source = st.radio(
+    "How do you want to select your colour?",
+    ["Manual Hexvalue", "Pixelcolour from an uploaded image"]
+)
 
-st.subheader("Colour distances")
-rgb_distance = calculate_rgb_euclidian_dist(colour1,colour2)
-hsv_distance = calculate_hsv_dist(colour1,colour2)
-cie76_distance = calculate_cie76_distance(colour1,colour2)
-ciede2000_distance = calculate_ciede2000_distance(colour1,colour2)
+if colour2_source == "Manual Hexvalue":
+    colour2_input = st.color_picker("Choose Colour 2", "#C11111")
+    colour2 = Colour(colour2_input)
+else:
+    uploaded_image = st.file_uploader(
+        "Upload an image",
+        type=["png", "jpg", "jpeg"]
+    )
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image).convert("RGB")
+        coordinates = streamlit_image_coordinates(image)
+        if coordinates is not None:
+            x_coord = coordinates["x"]
+            y_coord = coordinates["y"]
+            pixel = image.getpixel((x_coord, y_coord))
+            hex_code_pixel = Colour.getHexFromRGB(pixel[0],pixel[1],pixel[2])
+            colour2 = Colour(hex_code_pixel)
+            st.write(f"Pixel Hexcode: {hex_code_pixel}")
 
-rgb_level, rgb_color = get_similarity_level(rgb_distance, RGB_THRESHOLDS)
-hsv_level, hsv_color = get_similarity_level(hsv_distance, HSV_THRESHOLDS)
-cie76_level, cie76_color = get_similarity_level(cie76_distance, CIE76_THRESHOLDS)
-ciede2000_level, ciede2000_color = get_similarity_level(ciede2000_distance, CIEDE2000_THRESHOLDS)
 
-col1, col2, col3, col4 = st.columns(4)
-with col1:
-    display_similarity("RGB Euclidean", rgb_distance, rgb_level, rgb_color)
-with col2:
-    display_similarity("HSV Distance", hsv_distance, hsv_level, hsv_color)
-with col3:
-    display_similarity("CIE76", cie76_distance, cie76_level, cie76_color)
-with col4:
-    display_similarity("CIEDE2000", ciede2000_distance, ciede2000_level, ciede2000_color)
+if colour2 is not None:
+    st.subheader("Selected colours")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Target Colour")
+        st.code(colour1.hex)
+    with col2:
+        st.write("Your Colour")
+        st.code(colour2.hex)
+
+    st.subheader("Colour distances")
+    rgb_distance = calculate_rgb_euclidian_dist(colour1,colour2)
+    hsv_distance = calculate_hsv_dist(colour1,colour2)
+    cie76_distance = calculate_cie76_distance(colour1,colour2)
+    ciede2000_distance = calculate_ciede2000_distance(colour1,colour2)
+
+    rgb_level, rgb_color = get_similarity_level(rgb_distance, RGB_THRESHOLDS)
+    hsv_level, hsv_color = get_similarity_level(hsv_distance, HSV_THRESHOLDS)
+    cie76_level, cie76_color = get_similarity_level(cie76_distance, CIE76_THRESHOLDS)
+    ciede2000_level, ciede2000_color = get_similarity_level(ciede2000_distance, CIEDE2000_THRESHOLDS)
+
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        display_similarity("RGB Euclidean", rgb_distance, rgb_level, rgb_color)
+    with col2:
+        display_similarity("HSV Distance", hsv_distance, hsv_level, hsv_color)
+    with col3:
+        display_similarity("CIE76", cie76_distance, cie76_level, cie76_color)
+    with col4:
+        display_similarity("CIEDE2000", ciede2000_distance, ciede2000_level, ciede2000_color)
 
 
